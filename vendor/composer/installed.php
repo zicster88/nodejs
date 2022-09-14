a04a6a906e7e41a4ccb4dd8511895f307ac74e66
Ananda Adella Syahputra <dasilva.adela72@gmail.com>
2022-09-14 15:13:53 +0700

    add all file

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'dcba8a7d4bd3d71944b2b52be0b0d3348fe109a9',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'dcba8a7d4bd3d71944b2b52be0b0d3348fe109a9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
